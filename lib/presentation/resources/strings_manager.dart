c40eb25fd8358bfb873e1dbe124f4547619b6844
FlutLab.io <>
2022-05-29 18:22:22 +0000

    Autosave

--- lib/presentation/resources/strings_manager.dart
+++ lib/presentation/resources/strings_manager.dart
@@ -1,3 +1,15 @@
 class AppStrings {
   static const String noRouteFound = "No Route Found";
+
+  // OnBoarding
+  static const String onBoardingTitle1 = "See The Best Course #1";
+  static const String onBoardingTitle2 = "See The Best Course #2";
+  static const String onBoardingTitle3 = "See The Best Course #3";
+  static const String onBoardingTitle4 = "See The Best Course #4";
+
+  static const String onBoardingSubTitle1 = "Tut App is an awesome flutter application using clean architecture #1";
+  static const String onBoardingSubTitle2 = "Tut App is an awesome flutter application using clean architecture #2";
+  static const String onBoardingSubTitle3 = "Tut App is an awesome flutter application using clean architecture #3";
+  static const String onBoardingSubTitle4 = "Tut App is an awesome flutter application using clean architecture #4";
+  static const String skip = "Skip";
 }
