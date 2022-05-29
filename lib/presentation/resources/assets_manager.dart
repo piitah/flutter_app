f174421b0494c09d5f2d65d68c0ccec0af3ca2cd
FlutLab.io <>
2022-05-29 19:55:24 +0000

    Autosave

--- lib/presentation/resources/assets_manager.dart
+++ lib/presentation/resources/assets_manager.dart
@@ -6,7 +6,7 @@ class ImageAssets {
   static const String onboardinglogo2 = "$IMAGE_PATH/onboarding_logo2.svg";
   static const String onboardinglogo3 = "$IMAGE_PATH/onboarding_logo3.svg";
   static const String onboardinglogo4 = "$IMAGE_PATH/onboarding_logo4.svg";
-  static const String hollowCircleIc = "$IMAGE_PATH/hollow_cirlce_ic.svg";
+  static const String hollowCircleIc = "$IMAGE_PATH/hollow_circle_ic.svg";
   static const String leftArrowIc = "$IMAGE_PATH/left_arrow_ic.svg";
   static const String rightArrowIc = "$IMAGE_PATH/right_arrow_ic.svg";
   static const String solidCircleIc = "$IMAGE_PATH/solid_circle_ic.svg";
